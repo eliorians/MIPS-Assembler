1c3eb1bfa278d94b88caf77de706c6b452f49c51
Eli Orians <eli.orians@gmail.com>
2023-09-21 12:39:29 -0400

    small edit

--- assembler.py
+++ assembler.py
@@ -1,8 +1,19 @@
 import sys
 
 def main():
+    
+    #get file
     filename = sys.argv[1:]
+    
+    #parse file into list of lines
+    parser(filename)
+
+    #convert each line of assembly to hex
+    
+
+def parser(filename):
     print(filename)
+    
 
 if __name__ == "__main__":
     main()
