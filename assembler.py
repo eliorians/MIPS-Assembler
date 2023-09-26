cebdbb3a8ca0da26656cdb3c99ec57ccfa7d5bed
Eli Orians <eli.orians@gmail.com>
2023-09-26 12:20:24 -0400

    file reading and writing

--- assembler.py
+++ assembler.py
@@ -1,19 +1,24 @@
+
 import sys
+import os
 
 def main():
-    
-    #get file
-    filename = sys.argv[1:]
-    
-    #parse file into list of lines
-    parser(filename)
 
-    #convert each line of assembly to hex
+    #get file and put each line into a string in list
+    inputFile = sys.argv[1]
+    lines = open(inputFile).read().splitlines()
+
     
 
-def parser(filename):
-    print(filename)
-    
+
+    #write to output file, places line then newline
+    outputFile = os.path.splitext(inputFile)[0]+'.hex'
+    output = open(outputFile, 'w')
+    for i in lines:
+        output.writelines(i + '\n')
+    #removes last new line
+    output.truncate(output.tell()-len(os.linesep))
+    output.close()
 
 if __name__ == "__main__":
     main()
