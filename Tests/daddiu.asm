ac3238e9f7b5ffc95fab3092e71cbcbbee4d4ca8
Eli Orians <oriansed@appstate.edu>
2023-10-05 13:39:13 -0400

    18 tests pass

---
+++ Tests/daddiu.asm
@@ -0,0 +1,24 @@
+#
+#   To test stalling when Int RS becomes full   
+#
+    daddiu r1 r1 10
+    daddiu r2 r2 20
+    daddiu r3 r3 30
+    daddiu r4 r4 40
+    daddiu r5 r5 50
+    daddiu r6 r6 60
+    daddiu r7 r7 70
+    daddiu r8 r8 80
+    daddiu r9 r9 90
+    daddiu r10 r10 100
+    daddiu r11 r11 200
+    daddiu r12 r12 300
+    daddiu r13 r13 400
+    daddiu r14 r14 500
+    daddiu r15 r15 600
+    daddiu r16 r16 700
+    daddiu r17 r17 800
+    daddiu r18 r18 900
+    daddiu r19 r19 1000
+    daddiu r20 r20 2000
+    halt
