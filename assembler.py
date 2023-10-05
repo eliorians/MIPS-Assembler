d85b4add9465b5ac118eaba201e983373856bdfb
Eli Orians <eli.orians@gmail.com>
2023-10-05 18:46:54 -0400

    fixed halt for real

--- assembler.py
+++ assembler.py
@@ -342,7 +342,7 @@ def main():
             binaryLine = '000001'+decimal_to_binary26(0)
             binary.append(binaryLine)
             #if halt doesnt end on multiple of 8, make it so (but only if there are lines after!)
-            if ((pc+4) % 8 != 0 and i != len(lines[i])):
+            if ((pc+4) % 8 != 0 and lines[i] != lines[-1]):
                 binaryLine = decimal_to_binary32(0)
                 binary.append(binaryLine)
                 comments.append('')
